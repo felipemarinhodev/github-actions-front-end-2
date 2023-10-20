3d74bb47f01717390262bc996d8882d2dac51401
Felipe Marinho <github@felipemarinho.dev>
2023-10-19 18:47:28 -0700

    a3.2 - add deploy preview

---
+++ scripts/pullRequestPreview.js
@@ -0,0 +1,10 @@
+import { execSync } from "child_process";
+
+console.log("[DEPLOY_PREVIEW]: START");
+const command = "npm run deploy:staging";
+const output = execSync(command, { encoding: "utf-8" });
+console.log(output);
+console.log("[DEPLOY_PREVIEW]: END");
+
+// console.log("[GITHUB_COMMENT]: START");
+// console.log("[GITHUB_COMMENT]: END");
